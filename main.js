656113e8a32688cc5eeb765a603d1a2ec44c0d95
5Stygian <121655781+Blitzy5656@users.noreply.github.com>
2025-01-08 23:43:05 -0800

    did some js for once

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
+/* 
+you know what? STD (mr swaggs diss) lyrics
 
+Young enough to die
+And I'm rich enough to fuck
+Yeah I came from the bottom baby
+I'm not getting stuck
+We sip lean by the litre like (bubble bubble bubble)
+We sip lean by the litre like (bubble bubble bubble)
+Cooking king krab on the stove wit da formula
+Whippin Lamborghini in Bikini tryna run it up
+Sippin' on the leany while I'm leaning out the passenger
+Poppin' up on crabs like papparazzi ima snap him up
+It's like it's Biggie versus Tupac
+It's no biggie I smoke two packs uh
+My shit is cold it's a new track (Brrrr)
+I own the city and they knew that
+I'm eating burgers made of light yeah
+This my buzz and this my light year
+Fuck this bitch up with my woody
+I'm the kid just like I'm cudi
+MR Krabs you know I'm right here
+My bitch don't rag
+She a PC
+I got the mac
+I press delete
+I press the trigger at a Krab
+And I'm itchin for the cash
+Like I had an S T D
+No, you not a Krab
+You a rat boy
+Talkin' to police
+Huh? Fat boy?
+I catch him when he lackin'
+Out in traffic yeah my choppa automatic
+So you know it's gunna clap boy
+I got pearls on my neck
+I'm at the chum bucket
+I got pearl givin' neck
+Cause she a cum bucket
+You already sucked this dick
+Bitch you can't unsuck it
+Your dad a krab and I'm a demon he should run from me
+Young enough to die
+And I'm rich enough to fuck
+Yeah I came from the bottom baby
+I'm not getting stuck
+We sip lean by the litre like (bubble bubble bubble)
+We sip lean by the litre like (bubble bubble bubble)
+Cooking king krab on the stove wit da formula
+Whippin Lamborghini in Bikini tryna run it up
+Sippin' on the leany while I'm leaning out the passenger
+Poppin' up on crabs like papparazzi ima snap him up
+*/
